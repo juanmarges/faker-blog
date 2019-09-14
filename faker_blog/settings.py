ec5eabd9dcfbd2116da714ac49cda4c4a9e7dc79
Juan Marges <juanmarges97@gmail.com>
2019-09-13 21:46:33 -0400

    another heroku attempt

--- faker_blog/settings.py
+++ faker_blog/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Login
 LOGIN_REDIRECT_URL = '/'
