9766398a1701765008634c17465b94c5b5f186b2
Juan Marges <juanmarges97@gmail.com>
2019-09-13 21:52:12 -0400

    another heroku attempt

--- faker_blog/settings.py
+++ faker_blog/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
 # Login
 LOGIN_REDIRECT_URL = '/'
